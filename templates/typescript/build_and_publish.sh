78edc4c32a8383a9226d0a9f034a2f95f3f7d234
Kevin Schoonover <me@kschoon.me>
2020-07-26 15:09:21 -0700

    Fix typescript template not exporting from index.js using commonjs

--- templates/typescript/build_and_publish.sh
+++ templates/typescript/build_and_publish.sh
@@ -3,11 +3,14 @@ yarn install
 
 yarn build:proto
 
-for filename in ./dist/*.js; do
+module_files=`ls ./dist/*.js`
+
+mv index.js dist/
+
+for filename in $module_files; do
     module_name=`echo $(basename $filename) | cut -d"." -f1`
-    export_string="export * from \"./$module_name\";"
-    echo "$export_string" >> ./dist/index.js
-    echo "$export_string" >> ./dist/index.d.ts
+    echo "__export(require(\"./$module_name\"));" >> ./dist/index.js
+    echo "export * from \"./$module_name\";" >> ./dist/index.d.ts
 done
 
 yarn publish --non-interactive
